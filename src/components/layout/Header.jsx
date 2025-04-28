41e9e050863c4e8f094fd2b240af644d2d888ec3
Wapco-tech <wappo.dev@gmail.com>
2025-04-28 11:35:54 +0330

    Update Header.jsx

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -13,6 +13,7 @@ export const Header = () => {
           <li><Link to="/map">نقشه</Link></li>  
           <li><Link to="/qr-scan">اسکن QR</Link></li>  
           <li><Link to="/settings">تنظیمات</Link></li>  
+          <li><Link to="/login">ehi</Link></li>
         </ul>  
       </nav>  
     </header>  
