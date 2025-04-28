b66c3093dfdee8eeffce7ca9bf1db5b16d95f413
Wapco-tech <wappo.dev@gmail.com>
2025-04-28 11:31:50 +0330

    ch
    ch

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,36 @@
-import React from 'react';  
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';  
-import { HomePage } from './pages/HomePage';  
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HomePage } from './pages/HomePage';
 import MapPage from './pages/MapPage';
-import { QRScanPage } from './pages/QRScanPage';  
-import { SettingsPage } from './pages/SettingsPage';  
-import { NotFoundPage } from './pages/NotFoundPage';  
-import { Header } from './components/layout/Header';  
-import { Footer } from './components/layout/Footer'; 
+import { QRScanPage } from './pages/QRScanPage';
+import { SettingsPage } from './pages/SettingsPage';
+import { NotFoundPage } from './pages/NotFoundPage';
+import { LoginPage } from './pages/LoginPage'; // Added import
+import { Header } from './components/layout/Header';
+import { Footer } from './components/layout/Footer';
 //import { HashRouter as Router } from 'react-router-dom';  
-import './App.css';  
+import './App.css';
 
-function App() {  
-  return (  
-    <Router>  
-      <div className="app">  
-        <Header />  
-        <main className="main-content">  
-          <Routes>  
-            <Route path="/" element={<HomePage />} />  
-            <Route path="/map" element={<MapPage />} />  
-            <Route path="/qr-scan" element={<QRScanPage />} />  
-            <Route path="/settings" element={<SettingsPage />} />  
-            <Route path="/404" element={<NotFoundPage />} />  
-            <Route path="*" element={<Navigate to="/404" replace />} />  
-          </Routes>  
-        </main>  
-        <Footer />  
-      </div>  
-    </Router>  
-  );  
-}  
+function App() {
+  return (
+    <Router>
+      <div className="app">
+        <Header />
+        <main className="main-content">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/login" element={<LoginPage />} /> {/* Added route */}
+            <Route path="/map" element={<MapPage />} />
+            <Route path="/qr-scan" element={<QRScanPage />} />
+            <Route path="/settings" element={<SettingsPage />} />
+            <Route path="/404" element={<NotFoundPage />} />
+            <Route path="*" element={<Navigate to="/404" replace />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
 
-export default App;  
+export default App;
