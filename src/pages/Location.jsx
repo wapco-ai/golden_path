6b7e0d112da5530d124015ae5aad7ebe1d18006c
Wapco-tech <wappo.dev@gmail.com>
2025-05-18 21:13:46 +0330

    added
    added

--- src/pages/Location.jsx
+++ src/pages/Location.jsx
@@ -569,7 +569,10 @@ const Location = () => {
                     ></div>
                     <div className="place-info">
                       <h3 className="place-title">{event.title}</h3>
-                      <div className="place-address">{event.location}</div>
+                      <span className="place-address">
+                        <svg  xmlns="http://www.w3.org/2000/svg"  width="16"  height="16"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-door"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M14 12v.01" /><path d="M3 21h18" /><path d="M6 21v-16a2 2 0 0 1 2 -2h8a2 2 0 0 1 2 2v16" /></svg>
+                        {event.location}
+                      </span>
                       <div className="place-meta">
                         <span className="shrine-event-time">
                           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="currentColor">
