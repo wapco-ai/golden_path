936a4f90994080c0fd57bcbf6f192b07d050ce10
wapco <wappo.dev@gmail.com>
2025-06-29 18:52:02 +0330

    Improve i18n and direction handling

--- src/IntlProviderWrapper.jsx
+++ src/IntlProviderWrapper.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { IntlProvider } from 'react-intl';
 import { useLangStore } from './store/langStore';
 import fa from './locales/fa.json';
@@ -10,6 +10,10 @@ const messages = { fa, en, ar, ur };
 
 const IntlProviderWrapper = ({ children }) => {
   const language = useLangStore((state) => state.language);
+  useEffect(() => {
+    document.documentElement.lang = language;
+    document.documentElement.dir = language === 'en' ? 'ltr' : 'rtl';
+  }, [language]);
   return (
     <IntlProvider locale={language} messages={messages[language]} defaultLocale="fa">
       {children}
