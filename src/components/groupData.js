f425ce1ee242528da8868dcf2e673c545b1907d6
Wapco-tech <wappo.dev@gmail.com>
2025-06-29 18:40:16 +0330

    added
    added

--- src/components/groupData.js
+++ src/components/groupData.js
@@ -44,7 +44,7 @@ export const groups = [
   },  
   {   
     value: 'elmi',   
-    label: 'علمی/آموزشی/فرهنگی',  
+    label: 'فرهنگی',  
     icon: 'culture',  
     property: 'group',  
     svg: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor" stroke="currentColor" strokeWidth="2" strokeLinecap="round\" strokeLinejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 19a9 9 0 0 1 9 0a9 9 0 0 1 9 0"/><path d="M3 6a9 9 0 0 1 9 0a9 9 0 0 1 9 0"/><path d="M3 6l0 13"/><path d="M12 6l0 13"/><path d="M21 6l0 13"/></svg>`  
