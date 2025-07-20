6ba7763f4bc6016ebce71d3c8810f11dd625de09
wapco <wappo.dev@gmail.com>
2025-07-20 11:55:00 +0330

    fix react svg attribute warnings

--- src/pages/Routing.jsx
+++ src/pages/Routing.jsx
@@ -1074,7 +1074,23 @@ const RoutingPage = () => {
                 </div>
                 <div className="info-header">
                   <button className="close-button" onClick={() => navigate(-1)}>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-arrow-narrow-right"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M5 12l14 0" /><path d="M15 16l4 -4" /><path d="M15 8l4 4" /></svg>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      className="icon icon-tabler icons-tabler-outline icon-tabler-arrow-narrow-right"
+                    >
+                      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                      <path d="M5 12l14 0" />
+                      <path d="M15 16l4 -4" />
+                      <path d="M15 8l4 4" />
+                    </svg>
                   </button>
                   <div className="info-title">
                     <div className="info-stat">
