95f53191b333e1c58ee7cbe97daeac8b5d0bb65b
Wapco-tech <wappo.dev@gmail.com>
2025-08-16 18:06:02 +0330

    aded
    aded

--- src/pages/FinalSearch.jsx
+++ src/pages/FinalSearch.jsx
@@ -814,12 +814,12 @@ const FinalSearch = () => {
           />
         </div>
         <div className="info-details">
-          <div className="info-time">
+          {/* <div className="info-time">
             {getTransportIcon()}
             <span>
               {formatDigits(routeInfo.time)} <FormattedMessage id="minutesUnit" />
             </span>
-          </div>
+          </div> */}
           <div className="info-distance">
             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#181717" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
               <path stroke="none" d="M0 0h24v24H0z" fill="none" />
@@ -843,7 +843,7 @@ const FinalSearch = () => {
             <path stroke="none" d="M0 0h24v24H0z" fill="none" />
             <path d="M11.092 2.581a1 1 0 0 1 1.754 -.116l.062 .116l8.005 17.365c.198 .566 .05 1.196 -.378 1.615a1.53 1.53 0 0 1 -1.459 .393l-7.077 -2.398l-6.899 2.338a1.535 1.535 0 0 1 -1.52 -.231l-.112 -.1c-.398 -.386 -.556 -.954 -.393 -1.556l.047 -.15l7.97 -17.276z" />
           </svg>
-          <FormattedMessage id="startRouting" />
+          <FormattedMessage id="navigate" />
         </button>
         <button className="overview-btn" onClick={handleRouteOverview}>
           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#2196F3" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
