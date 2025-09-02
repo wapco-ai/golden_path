85b0c01c7f96a4673473f3ae0200909f2f5233e1
Wapco-tech <wappo.dev@gmail.com>
2025-09-02 13:39:17 +0330

    ad
    ade

--- src/pages/AdminPanel.jsx
+++ src/pages/AdminPanel.jsx
@@ -129,8 +129,12 @@ const AdminPanel = () => {
 
   return (
     <div className="admin-panel">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-      <meta name="theme-color" content="#000000" />
+      <Helmet>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+        <meta name="theme-color" content="#000000" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+      </Helmet>
       {/* Header */}
       <div className="admin-header">
         <div className="header-right">
