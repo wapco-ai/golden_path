01d85488b8a4838bec947ec18501b018e711f7a6
Wapco-tech <wappo.dev@gmail.com>
2025-09-29 11:06:15 +0330

    aded
    aded

--- src/pages/Location.jsx
+++ src/pages/Location.jsx
@@ -846,17 +846,16 @@ const Location = () => {
         <>
           <div className="modal-overlay" onClick={closeCommentModal}></div>
           <div className="comment-modal">
+            <div
+              className="search-bar-toggle6"
+              onClick={closeCommentModal}
+            >
+              <div className="toggle-handle6"></div>
+            </div>
             <div className="modal-header">
               <h3>
                 <FormattedMessage id="commentModalTitle" />
               </h3>
-              <button className="close-modal" onClick={closeCommentModal}>
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                  <path d="M18 6l-12 12" />
-                  <path d="M6 6l12 12" />
-                </svg>
-              </button>
             </div>
             <div className="modal-content">
               <p className="modal-instruction">
